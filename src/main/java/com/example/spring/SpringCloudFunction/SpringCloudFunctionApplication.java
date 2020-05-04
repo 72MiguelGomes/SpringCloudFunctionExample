0f76f654fda19bc90a751d44ebdcb78b77889dbd
Miguel Gomes <jmbg.41@gmail.com>
2020-05-04 22:47:03 +0100

    Add different types

--- src/main/java/com/example/spring/SpringCloudFunction/SpringCloudFunctionApplication.java
+++ src/main/java/com/example/spring/SpringCloudFunction/SpringCloudFunctionApplication.java
@@ -1,12 +1,19 @@
 package com.example.spring.SpringCloudFunction;
 
+import java.time.LocalTime;
+import java.util.function.Consumer;
 import java.util.function.Function;
+import java.util.function.Supplier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringCloudFunctionApplication {
+
+	private static final Logger logger = LoggerFactory.getLogger(SpringCloudFunctionApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCloudFunctionApplication.class, args);
@@ -17,4 +24,13 @@ public class SpringCloudFunctionApplication {
 		return name -> "Hello, " + name;
 	}
 
+	@Bean
+	public Consumer<String> saveName() {
+		return name -> logger.info("Save Name [Name={}]", name);
+	}
+
+	@Bean
+	public Supplier<String> timeNow() {
+		return () -> LocalTime.now().toString();
+	}
 }
