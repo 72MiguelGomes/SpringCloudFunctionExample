b3b82d1cac0a00bfbf9a4edc2f3e843ba6068f89
Miguel Gomes <jmbg.41@gmail.com>
2020-05-04 23:01:13 +0100

    Consumer defined by a class

---
+++ src/main/java/com/example/spring/SpringCloudFunction/functions/HealthCheck.java
@@ -0,0 +1,16 @@
+package com.example.spring.SpringCloudFunction.functions;
+
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+@NoArgsConstructor
+@Setter
+public class HealthCheck {
+
+    private int id;
+
+    private String name;
+
+}
---
+++ src/main/java/com/example/spring/SpringCloudFunction/functions/HealthCheckConsumer.java
@@ -0,0 +1,17 @@
+package com.example.spring.SpringCloudFunction.functions;
+
+import java.util.function.Consumer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class HealthCheckConsumer implements Consumer<HealthCheck> {
+
+    private static final Logger logger = LoggerFactory.getLogger(HealthCheckConsumer.class);
+
+    @Override
+    public void accept(HealthCheck data) {
+        logger.info("Consume HealthCheck [name={}]", data);
+    }
+}
