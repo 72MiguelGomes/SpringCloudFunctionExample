59e377fc35c9e1a1f03db9dfc5b7674e1ee694e7
Miguel Gomes <jmbg.41@gmail.com>
2020-05-04 22:41:15 +0100

    Create Hello world function

---
+++ README.md
@@ -0,0 +1,9 @@
+# Spring Cloud Function Example
+
+This project is a simple example how to use spring cloud function.
+
+## How to test
+
+```bash
+curl localhost:8080/sayHello -H "Content-Type: text/plain" -d "Miguel"
+```
--- src/main/java/com/example/spring/SpringCloudFunction/SpringCloudFunctionApplication.java
+++ src/main/java/com/example/spring/SpringCloudFunction/SpringCloudFunctionApplication.java
@@ -1,13 +1,20 @@
 package com.example.spring.SpringCloudFunction;
 
+import java.util.function.Function;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringCloudFunctionApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCloudFunctionApplication.class, args);
+	}
+
+	@Bean
+	public Function<String, String> sayHello() {
+		return name -> "Hello, " + name;
 	}
 
 }
