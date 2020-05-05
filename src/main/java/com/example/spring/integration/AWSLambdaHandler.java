2bbec74f4a4a223c7adb335f562bc8f2928b512a
Miguel Gomes <jmbg.41@gmail.com>
2020-05-05 21:36:08 +0100

    Add AWS Handler

---
+++ src/main/java/com/example/spring/integration/AWSLambdaHandler.java
@@ -0,0 +1,7 @@
+package com.example.spring.integration;
+
+import org.springframework.cloud.function.adapter.aws.SpringBootRequestHandler;
+
+public class AWSLambdaHandler extends SpringBootRequestHandler<String, String> {
+
+}
